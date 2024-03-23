960e571b0db24ea93925a177dbd8ee69a67ada3f
Rajdeep Ghosh <ghoshrajdeep2000@gmail.com>
2024-03-23 23:43:10 +0530

    upd

--- scratch_work/bs.py
+++ scratch_work/bs.py
@@ -1,47 +1,57 @@
+# from a particular commit, it scraps the + and - lines of code from the html file
+
 from bs4 import BeautifulSoup
 
-# Read the HTML file
-with open('w.html', 'r') as file:
-    html_content = file.read()
+def get_all(filename):
+    with open(f'../all_commits/{filename}', 'r') as file:
+        html_content = file.read()
 
-soup = BeautifulSoup(html_content, 'html.parser')
-
-
-trs = soup.find_all('tr', attrs={'data-hunk': True})
+    soup = BeautifulSoup(html_content, 'html.parser')
 
 
-# for tr in trs:
-    # print(tr)
-# print(len(trs))
+    trs = soup.find_all('tr', attrs={'data-hunk': True})
 
 
-data_list = []
-
-for tr in trs:
-    span = tr.find('span', class_='blob-code-inner')
-    content = span.get_text(strip=True)
-    data_code_marker = span['data-code-marker'] if 'data-code-marker' in span.attrs else ''
-
-    # print(f"{data_code_marker} {content}")
-    data_dict = {'content': content, 'data-code-marker': data_code_marker}
-    data_list.append(data_dict)
+    # for tr in trs:
+        # print(tr)
+    # print(len(trs))
 
 
-    # print('-' * 50)
-# print(data_list[3])
+    data_list = []
 
-for i in range(len(data_list)):
-    code= data_list[i]['content']
-    code = code.strip()
-    for j in range(10):
-        code = code.replace('  ', ' ')
-        code= code.replace('\n', ' ')
-    data_list[i]['content'] = code
-    # print(code)
-    # print('-' * 50)
-for i in range(len(data_list)):
-    print(f"{data_list[i]}")
-    print('-' * 50)
+    for tr in trs:
+        span = tr.find('span', class_='blob-code-inner')
+        content = span.get_text(strip=True)
+        data_code_marker = span['data-code-marker'] if 'data-code-marker' in span.attrs else ''
+
+        # print(f"{data_code_marker} {content}")
+        data_dict = {'content': content, 'data-code-marker': data_code_marker}
+        data_list.append(data_dict)
 
 
+        # print('-' * 50)
+    # print(data_list[3])
 
+    for i in range(len(data_list)):
+        code= data_list[i]['content']
+        code = code.strip()
+        for j in range(10):
+            code = code.replace('  ', ' ')
+            code= code.replace('\n', ' ')
+        data_list[i]['content'] = code
+        # print(code)
+        # print('-' * 50)
+    for i in range(len(data_list)):
+        print(f"{data_list[i]}")
+        print('-' * 50)
+
+
+
+with open('all_files_names.txt', 'r') as file:
+    filenames = file.read().splitlines()
+    # print(filenames)
+    for filename in filenames[:2]:
+        print(f"Processing {filename}")
+        get_all(filename)
+        print('-' * 50)
+        print('\n')
--- scratch_work/gh.py
+++ scratch_work/gh.py
@@ -1,30 +1,69 @@
 import requests
-
+from config import API_KEY
+token=API_KEY
 def get_commits(repo_owner, repo_name):
-    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/commits"
-    response = requests.get(url)
+    # url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/commits"
+    # response = requests.get(url)
     
-    if response.status_code == 200:
-        commits = response.json()
-        return commits
-    else:
-        print(f"Failed to fetch commits. Status code: {response.status_code}")
-        return None
+    # if response.status_code == 200:
+    #     commits = response.json()
+    #     return commits
+    # else:
+    #     print(f"Failed to fetch commits. Status code: {response.status_code}")
+    #     return None
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/commits"
+    params = {'per_page': 100}  # Request up to 100 commits per page
+    headers = {'Authorization': f'token {token}'}
+    commits = []
+
+    while url:
+        response = requests.get(url, params=params, headers=headers)
+        if response.status_code == 200:
+            # commits += response.json()
+            commits.extend(response.json())
+            # Check if there are more pages to fetch
+            url = response.links.get('next', {}).get('url')
+        else:
+            print(f"Failed to fetch commits. Status code: {response.status_code}")
+            return None
+
+    return commits
+
+def download_webpage(url,name):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            # with open('webpage.html', 'wb') as file:
+            with open(f'../all_commits/{name}', 'wb') as file:
+                file.write(response.content)
+            print(f"Webpage downloaded successfully as '{name}'")
+        else:
+            print(f"Failed to download webpage. Status code: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error downloading webpage: {e}")
+
 
 # Example usage
 repo_owner = "npm"
 repo_name = "cli"
 commits = get_commits(repo_owner, repo_name)
 
-all_fix=[]
+fixed_commit_ur=f'https://github.com/{repo_owner}/{repo_name}/commit/'
+
+all_files=[]
 c=0
 if commits:
     print(f"Commits count: {len(commits)}")
     for commit in commits:
-        # print(commit)
-        # print(f"Commit SHA: {commit['sha']} - Message: {commit['commit']['message']}")
+        # print(commit['sha'], commit['commit']['message'])
         if 'fix' in commit['commit']['message'].lower():
-            all_fix.append(commit['commit']['message'])
-            c+=1
-
-print(f"Total number of commits with 'fix' in the message: {c}")
+            url=fixed_commit_ur+commit['sha']
+            filename_commit=commit['sha']+'.html'
+            all_files.append(filename_commit)
+            # download_webpage(url,filename_commit)
+            
+print(len(all_files))
+# with open('all_files_names.txt', 'w') as file:
+#     for filename in all_files:
+#         file.write(f"{filename}\n")
+#     print(f"All filenames written to 'all_files.txt'")
