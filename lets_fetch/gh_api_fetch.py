cdbe176d335675eee9554939f48e4baaa9dde4b4
Rajdeep Ghosh <ghoshrajdeep2000@gmail.com>
2024-03-28 13:50:26 +0530

    upd

--- lets_fetch/gh_api_fetch.py
+++ lets_fetch/gh_api_fetch.py
@@ -39,11 +39,13 @@ def get_30_commits(repo_owner, repo_name):
 def download_webpage(url,name,org_name):
     try:
         response = requests.get(url)
+        if '/' in org_name:
+            org_name=org_name.replace('/','_')
         if response.status_code == 200:
             # with open('webpage.html', 'wb') as file:
             # create a folder named all_commits in the current directory
-            os.makedirs(f'../{org_name}', exist_ok=True)
-            with gzip.open(f'../{org_name}/{name}.gz', 'wb') as file:
+            os.makedirs(f'webpages/{org_name}', exist_ok=True)
+            with gzip.open(f'webpages/{org_name}/{name}.gz', 'wb') as file:
                 file.write(response.content)
             print(f"Webpage downloaded successfully as '{name}'")
         else:
@@ -54,16 +56,19 @@ def download_webpage(url,name,org_name):
 def flow(repo_owner, repo_name,org_name):
     
     commits = get_all_commits(repo_owner, repo_name)
-
+    if '/' in org_name:
+        org_name=org_name.replace('/','_')
     fixed_commit_ur=f'https://github.com/{repo_owner}/{repo_name}/commit/'
-    print(fixed_commit_ur)
+    # print(fixed_commit_ur)
     all_files=[]
-    c=0
+    commit_count=len(commits)
+    commit_count_fix=0
     if commits:
         print(f"Commits count: {len(commits)}")
         for commit in commits:
             # print(commit['sha'], commit['commit']['message'])
             if 'fix' in commit['commit']['message'].lower():
+                commit_count_fix+=1
                 url=fixed_commit_ur+commit['sha']
                 filename_commit=commit['sha']+'.html'
                 all_files.append(filename_commit)
@@ -71,11 +76,13 @@ def flow(repo_owner, repo_name,org_name):
                 
     # print(len(all_files))
     if len(all_files)>0:
-        with open(f'{org_name}_file_names.txt', 'w') as file:
+        with open(f'filenames/{org_name}.txt', 'w') as file:
             for filename in all_files:
                 file.write(f"{filename}\n")
-                
-        # print(f"All filenames written to 'all_files.txt'")
+    return commit_count, commit_count_fix
+    # with open("summary.txt", "a") as file:
+        # file.write(f"{repo_name} {org_name} {commit_count} {commit_count_fix}\n")
+
 
 start_time_global = datetime.now()
 
@@ -83,7 +90,7 @@ gh_links = []
 serial=1
 with open(f'o{serial}.txt', "r") as file:
     all_files = file.read().splitlines()
-    for line in all_files[:2]:
+    for line in all_files[:10]:
         temp=[]
         temp.append(line.split()[0])
         temp.append(line.split()[1])
@@ -96,9 +103,14 @@ for link in gh_links:
     repo_name=link[2]
     org_name=link[0]
     print(repo_owner,repo_name,org_name)
-    flow(repo_owner, repo_name,org_name)
+    # flow(repo_owner, repo_name,org_name)
+    commit_count, commit_count_fix = flow(repo_owner, repo_name,org_name)
     end_time = datetime.now()
     print(f"Time taken: {end_time - start_time}")
+    # print(f"Total commits with 'fix' keyword: {commit_count_fix}")
+    with open("status.txt", "a") as file:
+        file.write(f"{repo_name} {org_name} {end_time - start_time} {commit_count} {commit_count_fix}\n")
+    print("............................................")
     
 
 end_time_global = datetime.now()
